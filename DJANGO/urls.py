d1cad62fb8bf70c646737f483a0e7ba0a7047e5c
Arpan Hait <arpanhait2006@gmail.com>
2026-07-03 22:59:55 +0530

    Allow Django to serve media uploads in production when DEBUG is False

--- DJANGO/urls.py
+++ DJANGO/urls.py
@@ -24,8 +24,13 @@ urlpatterns = [
     path('api/', include('accounts.api_urls')),
     path('', include('home.urls')),
     path('', include('accounts.urls')),
-    path('api/', include('accounts.api_urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 
